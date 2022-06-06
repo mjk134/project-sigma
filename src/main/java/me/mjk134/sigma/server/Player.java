746868b79643cc5f466bc9d4c415ae9d89d76233
mjk 134 <57556877+mjk134@users.noreply.github.com>
2022-06-06 18:43:55 +0100

    Removal of storing coords of player when switching custom dimensions

--- src/main/java/me/mjk134/sigma/server/Player.java
+++ src/main/java/me/mjk134/sigma/server/Player.java
@@ -6,29 +6,13 @@ public class Player {
 
     public int numLives;
     public boolean allowsDonations;
-    public Vec3d lastOverworldCoords = new Vec3d(0,0,0);
-    public Vec3d lastNetherCoords = new Vec3d(0,0,0);
 
     public Player(int numLives, boolean allowsDonations) {
         this.numLives = numLives;
         this.allowsDonations = allowsDonations;
     }
 
-    public Vec3d getLastOverworldCoords() {
-        return lastOverworldCoords;
-    }
 
-    public void setLastOverworldCoords(Vec3d lastOverworldCoords) {
-        this.lastOverworldCoords = lastOverworldCoords;
-    }
-
-    public Vec3d getLastNetherCoords() {
-        return lastNetherCoords;
-    }
-
-    public void setLastNetherCoords(Vec3d lastNetherCoords) {
-        this.lastNetherCoords = lastNetherCoords;
-    }
 
     public int getNumLives() {
         return numLives;
